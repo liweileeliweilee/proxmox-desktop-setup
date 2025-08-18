52065a673e8dd5644d2d22d2ac92097e7c0bbc71
liweileeliweilee <61694171+liweileeliweilee@users.noreply.github.com>
2025-08-19 03:24:22 +0800

    Update proxmox-desktop-setup.sh
    echo "==== 安裝硬體監控工具 ===="
    apt install -y xfce4-taskmanager xfce4-systemload-plugin htop glances

--- proxmox-desktop-setup.sh
+++ proxmox-desktop-setup.sh
@@ -50,7 +50,7 @@ apt install -y vim-gtk3
 #apt install -y libheif-dev
 
 echo "==== 安裝硬體監控工具 ===="
-apt install -y intel-gpu-tools intel-media-va-driver vainfo nvtop lm-sensors psensor gnome-system-monitor xfce4-sensors-plugin
+apt install -y xfce4-taskmanager xfce4-systemload-plugin htop glances intel-gpu-tools intel-media-va-driver vainfo nvtop lm-sensors psensor gnome-system-monitor xfce4-sensors-plugin
 sensors-detect --auto || true
 
 echo "==== 安裝Thunar browse smb目錄工具 ===="
